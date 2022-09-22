aea60c1f68dfcea1cf2d36418e791679eb37c001
RIKO910 <tarikulislamriko910@gmail.com>
2022-09-22 18:04:25 +0600

    cart work

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Cart = (props) => {
+    return (
+        <div>
+            <p>Selected cart:{props.cart}</p>
+        </div>
+    );
+};
+
+export default Cart;
+
+
+
--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './shop.css'
 
@@ -31,7 +32,8 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <p>Selected cart:{cart.length}</p>
+                {/* <p>Selected cart:{cart.length}</p> */}
+                <Cart cart={cart.length}></Cart>
             </div>
         </div>
     );
